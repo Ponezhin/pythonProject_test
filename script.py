fe9f7e86e3e4e039b17b6b05ee7b2ccc762fde84
Ponezhin <mr.ponezhin@mail.ru>
2022-04-25 12:09:25 +0300

    Initial commit

---
+++ script.py
@@ -0,0 +1,38 @@
+# num = int(input("Введите число"))
+# if num % 2 == 0:
+#     print("четное число", num)
+# else:
+#     print("нечетное число")
+
+# letter = input('input text')
+# if letter == 'a' or letter == 'e' or letter == 'i' or letter == 'o' or letter == 'u':
+#     print('гласная буква')
+# elif letter == 'y':
+#     print('согласная и гласная буква')
+# else:
+#     print('согласная буква')
+
+# num = int(input())
+# data = []
+# while num != 0:
+#     data.append(num)
+#     num = int(input())
+# data.sort()
+# print(data)
+
+# words = input()
+# data = []
+# while words != '':
+#     if words not in data:
+#         data.append(words)
+#     words = input()
+# print(data)
+#
+# for item in data:
+#     print(item)
+
+# class Person:
+#     def sayHello (self, message):
+#         print(message)
+# tom = Person ()
+# tom.sayHello('Hello')
---
+++ script2.py
@@ -0,0 +1,111 @@
+# first_name = input("Введите ваше имя:")
+# last_name = input("Введите вашу фамилию:")
+# age = input("Введите ваш возраст:")
+# city = input("Введите город проживания:")
+#
+# # Выводим пустую строку
+# print("")
+#
+# # Выводим приветствие, подставляя имя и фамилию пользователя,
+# # которые он ввел с клавиатуры
+# print("Привет,", first_name, last_name, "!")
+#
+# # Выводим пустую строку
+# print("")
+#
+# # Выводим фиксированный текст для удобства просмотра
+# print("Ваш профиль:")
+#
+# # Выводим возраст и город, которые указал пользователь
+# print("Возраст:", age, 'лет')
+# print("Город:", city)
+
+# a = 1
+# b = 73
+# c = -12
+# d = 2
+# print(type(a))
+# # <class 'int'>
+# print(type(b))
+# # <class 'int'>
+# print(type(c))
+# # <class 'int'>
+# print(type(d))
+# # <class 'int'>
+#
+# e = a+b
+# print(e)
+# print(type(e))
+# # 74
+# # <class 'int'>
+#
+# f = b*c
+# print(f)
+# print(type(f))
+# # -876
+# # <class 'int'>
+#
+# g = b**d # оператор возведения в степень в python обозначается как **
+# print(g)
+# print(type(g))
+# # 5329
+# # <class 'int'>
+
+# x = 0.1
+# y = 21.5
+#
+# print(type(x))
+# # <class 'float'>
+# print(type(y))
+# # <class 'float'>
+#
+# z = y / x
+# print(z)
+# print(type(z))
+# # 215.0
+# # <class 'float'>
+#
+# print (0.1+0.1*5-0.3*4)
+# print ((3.14+0.3)/2+0.15)
+
+# a = -13
+# b = 7
+#
+# a = a - b
+# print(a)
+#
+# b = a + b
+# print(a)
+#
+# a = b - a
+# print(a)
+
+
+# a = input("Введите первую строку: ") .split()
+# b = input("Введите вторую строку: ") .split()
+#
+# a_set, b_set = set(a), set(b) # используем множественное присваивание
+#
+# a_and_b = a_set.symmetric_difference(b_set)
+#
+# c = map(int, a_and_b)
+#
+# d = list(map(int, c))
+#
+# d.sort()
+#
+# print(d)
+
+
+# list_1 = ['a', 'b', 'c']
+# list_2 = list_1
+# list_3 = list(list_1)
+# print(list_1)
+# print(list_2)
+# print(list_3)
+# print(list_1 == list_2)
+# print(list_1 == list_3)
+# print(list_1 is list_2)
+# print(list_1 is list_3)
+
+
